1d6cde7cb2cf8b72933786658d5f6930bf60cfd1
KazanskiiPN <fokkoo@yandex.ru>
2021-09-10 14:24:43 +0300

    MainAdapter is  finished 1:51

--- app/src/main/java/com/example/lesson2_/ui/main/view/MainAdapter.kt
+++ app/src/main/java/com/example/lesson2_/ui/main/view/MainAdapter.kt
@@ -1,28 +1,43 @@
 package com.example.lesson2_.ui.main.view
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.lesson2_.R
+import com.example.lesson2_.ui.main.model.Weather
 
 // создаем ресайклер вью путем наследования от него
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
 
 
+    var weatherData: List<Weather> = listOf()
+        set(value) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        TODO("Not yet implemented")
-    }
+
+            field = value
+            notifyDataSetChanged() // для отображения не забыть
+        }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder =
+        MainViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.main_fragment_item, parent, false)
+        )
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(weatherData[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = weatherData.size
+
+    inner class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(weather: Weather) {
+            itemView.findViewById<TextView>(R.id.cityName).text = weather.city.name
+
+        }
     }
 
-    inner class MainViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
-    }
 }
