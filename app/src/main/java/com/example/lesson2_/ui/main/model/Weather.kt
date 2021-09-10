8379fc9dc876b4e92c798200406b5f1087357826
KazanskiiPN <fokkoo@yandex.ru>
2021-09-10 13:12:01 +0300

    Data City is added is done 1:37

--- app/src/main/java/com/example/lesson2_/ui/main/model/Weather.kt
+++ app/src/main/java/com/example/lesson2_/ui/main/model/Weather.kt
@@ -11,3 +11,13 @@ data class Weather (
 
 fun getDefaultCity(): City = City("Moscow",55.75,37.61)
 
+// в интерфейсе менять не будем поэтому не мутбл
+fun getWorldCities(): List <Weather> = listOf(
+    Weather(City("London",51.5,-0.12),1,1),
+    Weather(City("Tokio",35.5,139.12),2,2),
+)
+
+fun getRussianCities(): List <Weather> = listOf(
+    Weather(City("Moscow",55.7,37.12),-5,6),
+    Weather(City("Kazan",55.8,49.12),5,5),
+)
