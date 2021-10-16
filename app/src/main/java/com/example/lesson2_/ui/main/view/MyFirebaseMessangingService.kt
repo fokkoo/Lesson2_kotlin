e9f3d5c8623bae8259e37c3721a4aa2c54a31bf3
KazanskiiPN <fokkoo@yandex.ru>
2021-10-17 00:32:26 +0300

    53:50 Intent filter is addad to manifest for FB masseging

---
+++ app/src/main/java/com/example/lesson2_/ui/main/view/MyFirebaseMessangingService.kt
@@ -0,0 +1,17 @@
+package com.example.lesson2_.ui.main.view
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessangingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("MyFMessangingService","onNewToken= $token")
+    }
+}
